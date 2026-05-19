4ac6989f89fb30945c50fcba46b5ba0dd05055b2
LGhiglionee <lghiglione@uade.edu.ar>
2026-05-19 11:41:32 -0300

    Avances

---
+++ mongo/consultasBasicas.py
@@ -0,0 +1,7 @@
+#Obtener todos los caballos que sean entrenados P F YIU
+
+#Todos los caballos que ganaron alguna carrera
+
+#Caballos que pesan menos de 1000
+
+#
---
+++ mongo/mainMongo.py
@@ -0,0 +1,4 @@
+from conectarMongo import *;
+
+ConectarMongo("Cursus", "InfoHorses")
+
